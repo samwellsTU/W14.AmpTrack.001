9f53862b266d05e6c2ca892b4b10b55862a7a7dd
samwellsTU <111082260+samwellsTU@users.noreply.github.com>
2025-04-23 12:52:29 -0400

    4/23 inclass

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const myCtx = new AudioContext();
-const analyser = myCtx.createAnalyser()
-analyser.fftSize = 1024
+const analyser = myCtx.createAnalyser();
+analyser.fftSize = 1024;
 
-const saw = myCtx.createOscillator()
-saw.type = "sawtooth"
+const saw = myCtx.createOscillator();
+saw.type = "sawtooth";
 saw.frequency.value = 100;
 
-const filter = myCtx.createBiquadFilter()
-filter.type = "lowpass"
+const filter = myCtx.createBiquadFilter();
+filter.type = "lowpass";
 filter.q = 50;
 filter.frequency.value = 100;
 
-saw.connect(filter)
-filter.connect(myCtx.destination)
+saw.connect(filter);
+filter.connect(myCtx.destination);
 
-
-
-
+document.getElementById("start").addEventListener("click", async () => {
+  const micInput = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+  });
+  myCtx.resume();
+  saw.start();
+});
